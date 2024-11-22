aedfe939e735565edd76310a599af485aead4149
vincent <85801240+vincentcomfy@users.noreply.github.com>
2024-11-22 23:41:02 +0800

    added book and book adding functions

---
+++ Project_Library/scripts.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function book(title, author, page, isRead) {
+    this.title = title;
+    this.author = author;
+    this.page = page;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary(title, author, page, isRead) {
+    
+}
